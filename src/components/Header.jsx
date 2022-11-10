2ee9f27a7685be3bf48a73332c2d65bc7caf6220
Max <maximo20162016@gmail.com>
2022-11-10 01:12:20 -0300

    ya estuvo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <>
             <h1 className="font-black text-5xl text-center mx-auto md:w-2/3">
-                Seguimiento Pacientes <span className="text-indigo-600">Veterinaria.</span>
+                Seguimiento Pacientes <span className="text-indigo-600">Veterinaria...</span>
             </h1>
         </>
     )
